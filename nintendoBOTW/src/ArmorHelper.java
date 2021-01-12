d84f7fbe7ec79714d8d2ce93ab1de2c0d6dafe16
Zecoder <70232378+EzekielJRamirez@users.noreply.github.com>
2021-01-12 13:50:17 -0800

    Add files via upload

---
+++ nintendoBOTW/src/ArmorHelper.java
@@ -0,0 +1,11 @@
+public class ArmorHelper {
+//    parent class
+//    getter
+//    setter
+    // the descendants are zora, anceint, barb
+    String name = "help";
+
+    public void setName() {
+        name = "helper";
+    }
+}
---
+++ nintendoBOTW/src/ZoraClass.java
@@ -0,0 +1,10 @@
+public class ZoraClass extends ArmorHelper{
+    String name = "Zora";
+    int[] level = {3, 5, 8, 12, 20};
+
+//    public static void setString{}
+    //    public static void getString{}
+    public void setName() {
+        name = "Zora";
+    }
+}

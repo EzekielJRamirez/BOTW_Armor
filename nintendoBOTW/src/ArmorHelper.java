f577505314ac082dc62dba99e2300a6e4f301beb
Zecoder <70232378+EzekielJRamirez@users.noreply.github.com>
2021-01-13 11:45:38 -0800

    Update ArmorHelper.java

--- nintendoBOTW/src/ArmorHelper.java
+++ nintendoBOTW/src/ArmorHelper.java
@@ -1,11 +1,22 @@
 public class ArmorHelper {
-//    parent class
-//    getter
-//    setter
+    //    parent class
     // the descendants are zora, anceint, barb
     String name = "help";
+    int[] level = {1, 2, 3, 4, 5};
 
-    public void setName() {
-        name = "helper";
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public int getDefense(int i) {
+        return level[i];
+    }
+
+    public int[] getLevel() {
+        return level;
     }
 }
